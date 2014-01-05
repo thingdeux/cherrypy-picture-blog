4dbbdf3fd738de5435d889295ea3dbefd7efa642
Joshua Johnson <me@josh.land>
2014-01-05 15:31:41 -0800

    Implemented skeleton for processing deleted image

--- src/filesystem.py
+++ src/filesystem.py
@@ -8,13 +8,21 @@ def delete_file(filename):
 def get_queue_directory_list():
 	returned_list = []     
 
-	for image_file in os.listdir(locations.queue_save_location()):
+	for image_file in os.listdir(locations.queue_save_location()):		
 	    filename_without_extension = image_file.split('.')[0]
 	    location = os.path.join(locations.queue_save_location(), image_file)
+	    #Create list with the filename (for displaying on the site) and actual file system location (for backend work)
+	    inner_list = []
+	    inner_list.append(filename_without_extension)
+	    inner_list.append(location)
 	    
-	    returned_list.append(filename_without_extension)
+	    returned_list.append(inner_list)
 
 	return (returned_list)	
+
+
+
+
 
 def get_queued_directory_locations():
 	returned_list = []     
