63d17702881ef1d08afb431f39d4a99a1ecef12e
thingdeux <uberbreaker@gmail.com>
2014-01-18 04:18:45 -0800

    Added JS Tag filter (windows will only show once you've selected applicable tag)

--- js/process.js
+++ js/process.js
@@ -1,17 +1,45 @@
-function silentlySendDataWithPost(location, data)
-{				
+function silentlySendDataWithPost(location, data) {				
 	$.post(location, data);
+}
+
+function hideUnusedTagBoxes() {
+	$(".sub_tag_info").hide();
+	$(".event_tag_info").hide();
+	$(".sub_tag_values").hide();
+	$(".event_tag_values").hide();
+}
+
+function hideOrShowTagBoxes(selectedTable, tagType, optionJQueryObject) {	
+	var tagName = optionJQueryObject.value;
+
+	if (tagType == "tag_selection")
+	{		
+		//selectedTable.find(".sub_tag_values").hide();
+		selectedTable.find(".sub_tag_values:contains(" + tagName + ")").show();
+		selectedTable.find(".sub_tag_info").show(200);
+
+	}
+	else if (tagType == "sub_tag_selection")
+	{	
+		var splitTags = tagName.split(";");		
+
+		//selectedTable.find(".event_tag_values").hide();
+		selectedTable.find(".event_tag_values:contains(" + splitTags[1] + ")").show();
+		selectedTable.find(".event_tag_info").show(200);
+	}
 }
 
 
 
 
 $(document).ready(function() {
-		
-	//Handler for 'process' button being clicked - will pass the form data over to cherrypy.
-	$(":button").click(function() {
+	hideUnusedTagBoxes();
+	
 
+	//Handler for any button being clicked - will pass the form data over to cherrypy.		
+	$(":button").click(function() {
 		
+		//'process' button
 		if ( $(this).is("#process_picture_button") )
 		{
 			silentlySendDataWithPost("/processPicture", $(this.form).serializeArray() );			
@@ -22,11 +50,28 @@ $(document).ready(function() {
 			if (verifyDelete)
 			{
 				silentlySendDataWithPost("/deletePicture", $(this.form).serializeArray() );
-			} //On cancel do nothing
+			}
 
 		}	
-
-		
 	});
+
+
+	//When a tag option box is selected
+	$("select").change(function() {
+		
+		//Iterate over each of the selected boxes
+		$( "select option:selected" ).each(function() {
+			//Walk up the DOM and find the main table
+			var tagType = $(this).parents("select").attr("name");
+			var selectedOptionsTable = $(this).parentsUntil("table");
+			
+			//Walk back down and find the tag box to unhide		 			
+			hideOrShowTagBoxes( selectedOptionsTable, tagType, this );
+		});
+
+	});
+
+
+
 
 })
