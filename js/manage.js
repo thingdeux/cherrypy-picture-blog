5e327a5414720b0caa4d70a659b89b99dcc49eed
Joshua Johnson <me@josh.land>
2014-01-24 20:36:32 -0800

    Lots of updates: 	[Manage Page] 	Can directly query the DB and get back a mako template with all information in various forms. 	Image menu has image selection for editing/deleting. 	cherrypy correctly serving content from /images and /thumbnails 	Misc. fixes and improvements

--- js/manage.js
+++ js/manage.js
@@ -1,34 +1,46 @@
 $(document).ready(function() {	
+	$("#manage_tabs").tabs();
+	$(".ui_menu").menu();
+
 	function silentlySendDataWithPost(location, data) {				
-		$.post(location, data);
+		var returnedData = $.ajax({
+								type: "POST",
+								url: location,
+								data: data,								
+								contentType: 'application/x-www-form-urlencoded',
+								responseType: 'XMLHttpRequestResponseType'
+								});
+			
+		returnedData.done (function (response, textStatus, jqXHR) {			
+			$("#image_picture_menu").html(response);		
+		});
 	}
 
 	function parseTagsReceived(uiObj) {
-		
-		//console.log( $(uiObj).attr('main') );
-		//console.log( $(uiObj).attr('sub') );
-		//console.log( $(uiObj).attr('event') );
 
 			var tags_to_submit = {
 				main_tag: $(uiObj).attr('main'),
 				sub_tag: $(uiObj).attr('sub'),
 				event_tag: $(uiObj).attr('event')
 			}
-
 		return (  tags_to_submit  );
 	}
 
-	$("#manage_tabs").tabs();
-	$(".ui_menu").menu();
+	
+
+
+
+	
+
+
+	
+	//Reset the width on event tags (they're typically longer) - wordwrap looks wrong
 	$(".long_event_tag").width(200);
 
-
-
-
+	//JQUERY UI Handler for 'images tab'
 	$(".ui-menu").on( "menuselect", function(event, ui) {					
-		var parsedTags = parseTagsReceived( $(ui).attr('item') );		
-
-		silentlySendDataWithPost("/getPictures", parsedTags);
+		var parsedTags = parseTagsReceived( $(ui).attr('item') );
+		silentlySendDataWithPost("/getPictures/", parsedTags);
 	});
 
 
