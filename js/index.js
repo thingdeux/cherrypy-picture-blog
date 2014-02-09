65f1e404b0351a1a727fd1404bbe333e478141f0
Joshua Johnson <me@josh.land>
2014-02-08 21:07:28 -0800

    Edited design on the index page.

--- js/index.js
+++ js/index.js
@@ -2,23 +2,24 @@ $(document).ready(function() {
 
 	//  --Global Variables-- //
 	//Keep all elements from query in variable so DOM does not need to be crawled again.
-	var main_tags = $('.tag_preview_image');	
+	var main_tags = $('.tag_preview_image');
+	var nav_tag_list = $('#tag_nav').find('.tag');
 	//Global var for determining when the preview image is animating.
 	var isTransitioning = false;
 	//Slideshow Transition speed
 	var slideshowShuffleSpeed = 3500;	
 	var shuffleTimeOutObject = 0;
 
-	function hideOrShowPreviewImageByName(name) {		
+	function showPreviewImageByName(name) {		
 		
 		if (isTransitioning == false) {
 			main_tags.each(function () {			
 				if ( $(this).is(":visible") ) {					
-					$(this).fadeOut(200);			
-				}										
+					$(this).fadeOut(200, 'swing');
+				}									
 				if ($(this).attr('id') == name) {
-					$(this).delay(200).show(150);
-				}		
+					$(this).delay(200).show(150, 'swing');
+				}				
 			});
 			//Prevent multiple images from hiding/fading out at the same time.
 			setTransitioning()
@@ -26,18 +27,29 @@ $(document).ready(function() {
 		}		
 	}
 
-	function shufflePreviewImage() {		
+	function shufflePreviewImage() {
 			randomSelection = getRandomPreviewImage();
-			hideAllPreviewImages(300);			
+			fadeAllPreviewImagesOut(300);			
 
 			main_tags.each(function (index) {
 				if (index == randomSelection) {
-					$(this).show(300);
+					var nameToHighlight = $(this).attr('id');					
+					$(this).delay(300).show(300, 'swing');									
 				}
 			});
 
-			shuffleTimeOutObject = window.setTimeout(function() { shufflePreviewImage() }, slideshowShuffleSpeed);
-		
+			shuffleTimeOutObject = window.setTimeout(function() { shufflePreviewImage() }, slideshowShuffleSpeed);	
+	}
+
+	function highlightNavTag(name) {
+		nav_tag_list.each(function() {
+			if ( $(this).attr('name') == name) {
+				$(this).css('color', 'white')
+			}
+			else {
+				$(this).css('color', 'black');
+			}
+		});
 	}
 	
 	function setTransitioning() {
@@ -46,9 +58,17 @@ $(document).ready(function() {
 	
 	function hideAllPreviewImages(speed) {
 		if (speed == null) { speed = 0; }
-		
+
 		main_tags.each( function(index) {					
 			$(this).hide(speed);
+		});
+	}
+
+	function fadeAllPreviewImagesOut(speed) {
+		if (speed == null) { speed = 0; }
+		
+		main_tags.each( function(index) {					
+			$(this).fadeOut(speed);
 		});
 	}
 
@@ -78,8 +98,9 @@ $(document).ready(function() {
 	});
 	
 	
-	$('.tag').hover(function () {			
-		hideOrShowPreviewImageByName( $(this).attr('name')  )
+	$('.tag').hover(function () {		
+		showPreviewImageByName( $(this).attr('name')  )
+		
 		//Stop slideshow on hover
 		clearTimeout(shuffleTimeOutObject);
 	},
