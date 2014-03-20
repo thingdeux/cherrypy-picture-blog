c3a42498ae87bfadf88bd08644fe9f25a3970700
thingdeux <uberbreaker@gmail.com>
2014-03-20 01:29:01 -0700

    Created 'Latest Uploads' section on index page. Re-used modal code from event page, small tweaks for compatibility (hacky) Prevented previews.css from loading on index page as responsive rules foul up the model.

--- js/event.js
+++ js/event.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
 		var returnedData = $.ajax({
 			type: "GET",
 			url: '/getModalPicture',
-			data: { image_id: data }			
+			data: { 
+				image_id: data[0],
+				onIndex: data[1]
+			}			
 		});
 
 		returnedData.done(function ( response, textStatus, jqXHR) {						
@@ -12,11 +15,19 @@ $(document).ready(function() {
 		});		
 	}
 
-	$( ".nav_image" ).tooltip({ track: true });		
+	$( ".nav_image" ).tooltip({ track: true });	
 
-	$(".nav_image").click(function () {
-		image_id = $(this).children().find('img').prop('id');
-		getModal(image_id);		
+	$(".nav_image").click(function () {		
+		var image_id = $(this).children().find('img').prop('id');
+		var onIndex = undefined
+
+		//Ended up reusing this functionality (yay) so this is a 'hack' (boo) to make it work on the index page		
+		if (typeof image_id == 'undefined') {				
+			var image_id = $(this).attr('id')
+			var onIndex = true
+		}
+		
+		getModal( [image_id, onIndex] );	
 	});	
 
 });
